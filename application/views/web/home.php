0c2aa4c12db3743c2af7cb1d9a52a50c7d03d249
Andhiny Fatikha <andhinyfatikha@gmail.com>
2022-12-20 05:34:11 +0700

    first commit

---
+++ application/views/web/home.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- basic -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- mobile metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+  <!-- site metas -->
+  <title>Stay in Central Jakarta</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!-- fevicon -->
+  <link rel="icon" href="<?=base_url()?>assets/web/images/fevicon.png" type="image/gif" />
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="<?=base_url()?>assets/web/css/bootstrap.min.css">
+  <!-- style css -->
+  <link rel="stylesheet" href="<?=base_url()?>assets/web/css/style.css">
+  <!-- Responsive-->
+  <link rel="stylesheet" href="<?=base_url()?>assets/web/css/responsive.css">  
+  <!-- Scrollbar Custom CSS -->
+  <link rel="stylesheet" href="<?=base_url()?>assets/web/css/jquery.mCustomScrollbar.min.css">
+  <!-- Tweaks for older IEs-->
+  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/<?=base_url()?>assets/web/css/font-awesome.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+</head>
+<!-- body -->
+
+<body class="main-layout">
+  <!-- loader  -->
+  <div class="loader_bg">
+    <div class="loader"><img src="<?=base_url()?>assets/web/images/loading.gif" alt="#" /></div>
+  </div>
+  <!-- end loader -->
+  <!-- header -->
+  <header>
+    <!-- header inner -->
+    <div class="header-top">
+      <div class="header">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xl-2 col-lg-4 col-md-4 col-sm-3 col logo_section">
+              <div class="full">
+                <div class="center-desk">
+                  <div class="logo">
+                    <a href="index.html"><img src="<?=base_url()?>assets/web/images/hotel-logo.png" alt="#" /></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-10 col-lg-8 col-md-8 col-sm-9">
+              <div class="header_information">
+               <div class="menu-area">
+                <div class="limit-box">
+                  <nav class="main-menu ">
+                    <ul class="menu-area-main">
+                      <li class="active"> <a href="#myCarousel">Home</a></li>
+                      <li> <a href="#about">About</a> </li>
+                      <li> <a href="#map">Map</a> </li>
+                      <li> <a href="#contact">Contact</a> </li>
+                     </ul>
+                   </nav>
+                 </div>
+               </div> 
+               <div class="mean-last">
+                       <a href="#"><img src="<?=base_url()?>assets/web/images/search_icon.png" alt="#" /></a> <a href="#">Search</a></div>              
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+
+     <!-- start carousel -->
+     <section class="slider_section">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+
+            <div class="container-fluid padding_dd">
+              <div class="carousel-caption">
+                <div class="row">
+                  <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                    <div class="text-bg">
+                    <h1>Staycation in Jakarta</h1>
+                      <p>Be it for your weekend wind-down, or for a long staycays, you can find lots of fancy properties in the capital city — close from your home. Staying in a nice property that offers complete facilities such as an in-hotel dining option, spa, and swimming pool, can make you feel happy. After all, isn’t that what we’re looking for in life? Finding happiness? 🏨😁</p>
+                    </div>
+                  </div>
+                  <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                    <div class="images_box">
+                      <figure><img src="<?=base_url()?>assets/web/images/hotel-1.jpg"></figure>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+
+            <div class="container-fluid padding_dd">
+              <div class="carousel-caption">
+
+                <div class="row">
+                  <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                    <div class="text-bg">
+                      <h1>Jakarta is the best choice for your staycation!</h1>
+                      <p>Capital city of Indonesia and hub of this country's Government, trade, business finance and economy, Metropolitan Jakarta offers a wide range of luxurious hotels from those located in exclusive highrise buildings in the center of the city to lavish sized hotels on the outskirst of the city, hidden among green trees with stretching lawns. Since Jakarta is a sprawling city, it is wise to pick a hotel that is closest to your business needs, or you may waste time in the traffic.</p>
+                    </div>
+                  </div>
+
+                  <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                    <div class="images_box">
+                      <figure><img src="<?=base_url()?>assets/web/images/hotel-3.jpg"></figure>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+
+          <div class="carousel-item">
+
+            <div class="container-fluid padding_dd">
+              <div class="carousel-caption ">
+                <div class="row">
+                  <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                    <div class="text-bg">
+                    <h1>Lets find your Favorite Hotel in Jakarta</h1>
+                      <p>Staycation, Stay Fun</p>
+                    </div>
+                  </div>
+                  <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12">
+                    <div class="images_box">
+                      <figure><img src="<?=base_url()?>assets/web/images/hotel-4.jpeg"></figure>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+</section>
+</div>
+</header>
+<!-- end carousel -->
+
+<!-- about  -->
+<div id="about" class="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <div class="about-box">
+          <h2>About <strong class="yellow">Our Hotel</strong></h2>
+          <p> The most common type of accommodation in the hotel industry, a hotel is defined as an establishment that offers overnight accommodation, meals and other services. They are mainly aimed at travellers or tourists, although locals may also use them. Hotels provide private rooms, and almost always have en-suite bathrooms. </p>
+        </div>
+      </div>
+      <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+        <div class="about-box">
+          <figure><img src="<?=base_url()?>assets/web/images/hotel-2.jpg" alt="#" /></figure>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- end abouts -->
+
+<!-- start map -->
+<div id="map" class="map">
+  <br>
+<iframe src="<?=base_url()?>/home" width="1260" height="650"></iframe>
+</div>
+<!-- end map --> 
+
+<!--  footer -->
+<footer>
+  <div id="contact" class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 ">
+          <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6 ">
+              <div class="address">
+                <br>
+                <h3>Contact Us </h3>
+                <ul class="loca">
+                  <li>
+                    <a href="#"><img src="<?=base_url()?>assets/web/icon/loc.png" alt="#" /></a>DKI Jakarta
+                    <br>Indonesia</li>
+                    <li>
+                      <a href="#"><img src="<?=base_url()?>assets/web/icon/email.png" alt="#" /></a>andhiny.fr@gmail.com</li>
+                      <li>
+                        <a href="#"><img src="<?=base_url()?>assets/web/icon/call.png" alt="#" /></a>+6289601166772</li>
+                      </ul>
+                      <ul class="social_link">
+                        <li><a href="#"><img src="<?=base_url()?>assets/web/icon/fb.png"></a></li>
+                        <li><a href="#"><img src="<?=base_url()?>assets/web/icon/tw.png"></a></li>
+                        <li><a href="#"><img src="<?=base_url()?>assets/web/icon/lin(2).png"></a></li>
+                        <li><a href="#"><img src="<?=base_url()?>assets/web/icon/instagram.png"></a></li>
+                      </ul>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+          <div class="copyright">
+            <div class="container">
+              <p>Copyright © 2019 Created by <a href="https://www.linkedin.com/in/andhiny-fatikha/">Andhiny Fatikha</a></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="<?=base_url()?>assets/web/js/jquery.min.js"></script>
+      <script src="<?=base_url()?>assets/web/js/popper.min.js"></script>
+      <script src="<?=base_url()?>assets/web/js/bootstrap.bundle.min.js"></script>
+      <script src="<?=base_url()?>assets/web/js/jquery-3.0.0.min.js"></script>
+      <script src="<?=base_url()?>assets/web/js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="<?=base_url()?>assets/web/js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="<?=base_url()?>assets/web/js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+
+</body>
+
+</html>
